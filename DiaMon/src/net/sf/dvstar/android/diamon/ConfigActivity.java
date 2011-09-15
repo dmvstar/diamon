1c48215dceb407740b218a00c935e5964286d02e
dmvstar <dmvstar@gmail.com>
2011-09-15 21:10:06 +0000

    init all

---
+++ DiaMon/src/net/sf/dvstar/android/diamon/ConfigActivity.java
@@ -0,0 +1,14 @@
+package net.sf.dvstar.android.diamon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConfigActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.config);
+    }
+	
+}
