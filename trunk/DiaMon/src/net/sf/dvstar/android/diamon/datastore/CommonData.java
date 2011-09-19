59cb7fb3772c492c9686c3dd3293a138dc9fc255
dmvstar@gmail.com <dmvstar@gmail.com@7170698d-7e1a-ea82-3c62-e3c85c3ce9f1>
2011-09-19 14:59:54 +0000

    new objects

---
+++ trunk/DiaMon/src/net/sf/dvstar/android/diamon/datastore/CommonData.java
@@ -0,0 +1,12 @@
+package net.sf.dvstar.android.diamon.datastore;
+
+import android.graphics.Bitmap;
+
+public class CommonData {
+	public static class UserDesc {
+		public String id;
+	    public String name;
+	    public byte[] picture;
+	    public Bitmap pictureBitmap;
+	} 
+}
