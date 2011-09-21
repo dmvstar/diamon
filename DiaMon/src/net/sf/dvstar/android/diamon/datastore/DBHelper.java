3e377a6e55deb5c926907c9573f49d576bf80e38
dmvstar <dmvstar@gmail.com>
2011-09-21 09:49:53 +0000

    update ui

--- DiaMon/src/net/sf/dvstar/android/diamon/datastore/DBHelper.java
+++ DiaMon/src/net/sf/dvstar/android/diamon/datastore/DBHelper.java
@@ -1,66 +1,152 @@
 package net.sf.dvstar.android.diamon.datastore;
 
+import java.io.IOException;
+
 import android.content.Context;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Environment;
+import android.util.Log;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-	private SQLiteDatabase db;
-    private static final int DATABASE_VERSION = 1;
-    private static final String DB_NAME = "diamod.db";
-    private static final String TABLE_NAME = "profiles";
+	private static final String TAG                  = "DatabaseHelper";
 	
-	//public DBHelper(Context context, String name, CursorFactory factory,int version) {
-	public DBHelper(Context context){
-		super(context, DB_NAME, null, DATABASE_VERSION);
-        db = getWritableDatabase();	
-    }
+	private SQLiteDatabase diamondb;
+	private static final int DATABASE_VERSION = 1;
+	private static final String DB_NAME = "diamod.db";
+	private static final String TABLE_NAME = "profiles";
+	private static String DB_FULL_PATH = "";
 
-	@Override
-	public void onCreate(SQLiteDatabase db) {
-        db.execSQL(
-                "create table " + TABLE_NAME + " (_id integer primary key autoincrement, " +
-                " user_id text not null, user_name text not null) ");
+	// public DBHelper(Context context, String name, CursorFactory factory,int
+	// version) {
+	public DBHelper(Context context) {
+		super(context, getDBName(context), null, DATABASE_VERSION);
+		DB_FULL_PATH = getDBName(context);
+		try {
+            this.createDataBase();
+        } catch (IOException ioe) {
+            throw new Error("Unable to create database");
+        }
+		//db = getWritableDatabase();
 	}
 
-    /**
-     * The Insert DB statement
-     * @param id the friends id to insert
-     * @param name the friend's name to insert
-     */
-    public void insert(String user_id, String user_name) {
-        db.execSQL("INSERT INTO friends('user_id', 'user_name') values ('"
-                + user_id + "', '"
-                + user_name + "')");
+	/**
+     * Creates a empty database on the system and rewrites it with your own
+     * database.
+     * */
+    public void createDataBase() throws IOException {        
+        if (!checkDataBase()) this.getWritableDatabase();
     }
 
     /**
-     * Wipe out the DB
+     * Check if the database already exist to avoid re-copying the file each
+     * time you open the application.
+     * 
+     * @return true if it exists, false if it doesn't
      */
-    public void clearAll() {
-        db.delete(TABLE_NAME, null, null);
+    private boolean checkDataBase() {
+        SQLiteDatabase checkDB = null;
+        try {
+            checkDB = SQLiteDatabase.openDatabase(DB_FULL_PATH, null,
+                    SQLiteDatabase.OPEN_READONLY);
+        } catch (SQLiteException e) {
+            // database does't exist yet.
+        }
+        if (checkDB != null) {
+            checkDB.close();
+        }
+        return checkDB != null ? true : false;
     }
 
-    /**
-     * Select All the returns a Cursor
-     * @return the cursor for the DB selection
-     */
-    public Cursor cursorSelectAll() {
-        Cursor cursor = this.db.query(
-                TABLE_NAME, // Table Name
-                new String[] { "user_id", "user_name" }, // Columns to return
-                null,       // SQL WHERE
-                null,       // Selection Args
-                null,       // SQL GROUP BY
-                null,       // SQL HAVING
-                "name");    // SQL ORDER BY
-        return cursor;
+    public void openDataBase(int mode) throws SQLException {        
+        try {
+        	diamondb = SQLiteDatabase.openDatabase(DB_FULL_PATH, null, mode);
+        } catch(IllegalStateException e) {
+            // Sometimes, esp. after application upgrade, the database will be non-closed, raising a IllegalStateException
+            // below. Try to avoid by simply opening it again.
+            Log.d(TAG, "Database non-closed. Reopening.");
+            diamondb = SQLiteDatabase.openDatabase(DB_FULL_PATH, null, mode);
+        }
     }
 
+    public void openDataBase() throws SQLException {
+        openDataBase(SQLiteDatabase.OPEN_READWRITE);
+    }
+
+    public SQLiteDatabase getDb() {
+        return diamondb;
+    }
+
+    @Override
+    public synchronized void close() {
+        if (diamondb != null)
+        	diamondb.close();
+        super.close();
+    }
+
+    
+	private static String getDBName(Context context) {
+		String ret = DB_NAME;
+		try {
+			String dbSDPath = Environment.getExternalStorageDirectory()
+					.getPath()
+					+ "/Android/data/"
+					+ context.getPackageName()
+					+ "/databases/" + DB_NAME;
+			DB_FULL_PATH = dbSDPath; 
+		} catch (Exception e) {
+		}
+		return ret;
+	}
 	
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("create table " + TABLE_NAME
+				+ " (_id integer primary key autoincrement, "
+				+ " user_id text not null, user_name text not null) ");
+	}
+
+	/**
+	 * The Insert DB statement
+	 * 
+	 * @param id
+	 *            the friends id to insert
+	 * @param name
+	 *            the friend's name to insert
+	 */
+	public void insert(String user_id, String user_name) {
+		diamondb.execSQL("INSERT INTO friends('user_id', 'user_name') values ('"
+				+ user_id + "', '" + user_name + "')");
+	}
+
+	/**
+	 * Wipe out the DB
+	 */
+	public void clearAll() {
+		diamondb.delete(TABLE_NAME, null, null);
+	}
+
+	/**
+	 * Select All the returns a Cursor
+	 * 
+	 * @return the cursor for the DB selection
+	 */
+	public Cursor cursorSelectAll() {
+		Cursor cursor = this.diamondb.query(TABLE_NAME, // Table Name
+				new String[] { "user_id", "user_name" }, // Columns to return
+				null, // SQL WHERE
+				null, // Selection Args
+				null, // SQL GROUP BY
+				null, // SQL HAVING
+				"name"); // SQL ORDER BY
+		return cursor;
+	}
+
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// TODO Auto-generated method stub
