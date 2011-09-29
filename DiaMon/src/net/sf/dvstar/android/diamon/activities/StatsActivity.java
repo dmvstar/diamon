ae5c7bdcff46e79a01c7516d478014b8cf0a055b
dmvstar <dmvstar@gmail.com>
2011-09-29 16:53:10 +0000

    update ui

---
+++ DiaMon/src/net/sf/dvstar/android/diamon/activities/StatsActivity.java
@@ -0,0 +1,69 @@
+package net.sf.dvstar.android.diamon.activities;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.sf.dvstar.android.diamon.R;
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Spinner;
+
+import net.sf.dvstar.android.diamon.charts.*;
+
+public class StatsActivity extends ListActivity {
+
+  private IDemoChart[] mCharts = new IDemoChart[] { 
+      new BudgetPieChart(),
+      new MultipleTemperatureChart() 
+      };
+
+	
+  private String[] mMenuText;
+
+  private String[] mMenuSummary;
+
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    int length = mCharts.length;
+    mMenuText = new String[length];
+    mMenuSummary = new String[length];
+    for (int i = 0; i < length; i++) {
+      mMenuText[i] = mCharts[i].getName();
+      mMenuSummary[i] = mCharts[i].getDesc();
+    }
+    setListAdapter(new SimpleAdapter(this, getListValues(), android.R.layout.simple_list_item_2,
+        new String[] { IDemoChart.NAME, IDemoChart.DESC }, new int[] { android.R.id.text1,
+            android.R.id.text2 }));
+  }
+
+  private List<Map<String, String>> getListValues() {
+    List<Map<String, String>> values = new ArrayList<Map<String, String>>();
+    int length = mMenuText.length;
+    for (int i = 0; i < length; i++) {
+      Map<String, String> v = new HashMap<String, String>();
+      v.put(IDemoChart.NAME, mMenuText[i]);
+      v.put(IDemoChart.DESC, mMenuSummary[i]);
+      values.add(v);
+    }
+    return values;
+  }
+
+  @Override
+  protected void onListItemClick(ListView l, View v, int position, long id) {
+    super.onListItemClick(l, v, position, id);
+    Intent intent = null;
+    intent = mCharts[position].execute(this);
+    startActivity(intent);
+  }
+	
+}
