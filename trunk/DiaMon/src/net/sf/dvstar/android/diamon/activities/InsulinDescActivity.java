740e8baa585ea2b5c76d7aeef412b004c7c17606
dmvstar@gmail.com <dmvstar@gmail.com@7170698d-7e1a-ea82-3c62-e3c85c3ce9f1>
2011-10-07 13:11:58 +0000

    update ui

--- trunk/DiaMon/src/net/sf/dvstar/android/diamon/activities/InsulinDescActivity.java
+++ trunk/DiaMon/src/net/sf/dvstar/android/diamon/activities/InsulinDescActivity.java
@@ -6,12 +6,14 @@ import java.util.Map;
 
 import net.sf.dvstar.android.diamon.R;
 import net.sf.dvstar.android.diamon.datastore.CommonData;
+import net.sf.dvstar.android.diamon.datastore.DBConst;
 import net.sf.dvstar.android.diamon.datastore.DBHelper;
 import net.sf.dvstar.android.diamon.widgets.color.ColorSelectorDialog;
 import android.app.Activity;
 import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,17 +21,21 @@ import android.widget.AdapterView;
 import android.widget.CursorAdapter;
 import android.widget.EditText;
 import android.widget.SimpleAdapter;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemSelectedListener;
 
-public class InsulinDescActivity extends Activity {
+public class InsulinDescActivity extends Activity implements DBConst {
 
 	DBHelper dbHelper;
-	
+
 	final String[] from = new String[] { "insulinName", "workFrom", "workTo",
 			"maxFrom", "maxTo", "color" };
+	final String[] from_cur = new String[] { KEY_INSULIN_DESC,
+			KEY_INSULIN_TSTRT, KEY_INSULIN_TEND, KEY_INSULIN_TMAX,
+			KEY_INSULIN_TWRK, KEY_INSULIN_COLOR };
 	final int[] to = new int[] { android.R.id.text1, android.R.id.text2,
 			android.R.id.text2, android.R.id.text2, android.R.id.text2,
 			android.R.id.text2, android.R.id.text2 };
@@ -37,11 +43,23 @@ public class InsulinDescActivity extends Activity {
 	EditText editTextInsulinName, editTextInsulinFrom, editTextInsulinTo,
 			editTextInsulinMaxFrom, editTextInsulinMaxTo, editTextInsulinColor;
 
+	private Cursor insulinCursor;
+
+	private Spinner spinner;
+
+	private SimpleCursorAdapter cursorAdapter;
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		//if(insulinCursor!=null)	insulinCursor.close();
+	}
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.insulin_desc);
-
+		
 		editTextInsulinName = (EditText) findViewById(R.id.editTextInsulinName);
 		editTextInsulinFrom = (EditText) findViewById(R.id.editTextInsulinFrom);
 		editTextInsulinTo = (EditText) findViewById(R.id.editTextInsulinTo);
@@ -51,9 +69,12 @@ public class InsulinDescActivity extends Activity {
 
 		CommonData commonData = new CommonData();
 		dbHelper = new DBHelper(this);
-
-		final CursorAdapter cursorAdapter = new CursorAdapter(this, dbHelper
-				.getAlInsulins()) {
+		
+		insulinCursor = dbHelper.getAlInsulins();
+		startManagingCursor(insulinCursor);
+		
+		cursorAdapter = new SimpleCursorAdapter(this,
+				R.layout.insulin_item, insulinCursor, from_cur, to) {
 
 			/*
 			 * public CursorAdapter(Context context, Cursor c){ super(context,
@@ -72,25 +93,44 @@ public class InsulinDescActivity extends Activity {
 			public void bindView(View view, Context context, Cursor cursor) {
 				TextView summary = (TextView) view
 						.findViewById(R.id.editTextInsulinName);
-				summary.setText(cursor.getString(cursor
-						.getColumnIndex(DBHelper.KEY_INSULIN_DESC)));
+				String text = cursor.getString(cursor
+						.getColumnIndex(DBHelper.KEY_INSULIN_DESC));
+				//summary.setText( text );
+				//Log.d("DB", "bindView(["+text+"]["+summary.getId()+"])");
 			}
 
 			public View getView(int position, View convertView, ViewGroup parent) {
 				final View view = super.getView(position, convertView, parent);
 				Cursor cursor = getCursor();
 				int count = cursor.getCount();
+
+				Log.d("DB", "cursor(["+cursor.getCount()+"]["+cursor.getColumnCount()+"]) to ("+position+")");
 				
 				LayoutInflater inflater = getLayoutInflater();
 				View row = inflater.inflate(R.layout.insulin_item, parent,
 						false);
 
-				cursor.move(position);
-				
-				TextView label = (TextView) row.findViewById(R.id.InsulinName);
-				String text = cursor.getString(  cursor.getColumnIndex(  DBHelper.KEY_INSULIN_DESC ) );
-				label.setText( text );
+				//cursor.move(position);
 
+				TextView label = (TextView) row.findViewById(R.id.InsulinName);
+				String text = cursor.getString(cursor
+						.getColumnIndex(KEY_INSULIN_DESC));
+				label.setText(text);
+				Log.d("DB", "text ["+text+"]");
+
+				text = cursor.getString(cursor.getColumnIndex(KEY_INSULIN_TSTRT));
+				label = (TextView) row.findViewById(R.id.WorkFrom);
+				label.setText(text);
+				text = cursor.getString(cursor.getColumnIndex(KEY_INSULIN_TEND));
+				label = (TextView) row.findViewById(R.id.WorkTo);
+				label.setText(text);
+				text = cursor.getString(cursor.getColumnIndex(KEY_INSULIN_TMAX));
+				label = (TextView) row.findViewById(R.id.MaxFrom);
+				label.setText(text);
+				text = cursor.getString(cursor.getColumnIndex(KEY_INSULIN_TWRK));
+				label = (TextView) row.findViewById(R.id.MaxTo);
+				label.setText(text);
+				
 				return row;
 			}
 
@@ -99,23 +139,33 @@ public class InsulinDescActivity extends Activity {
 				View view = super
 						.getDropDownView(position, convertView, parent);
 				System.out.println(position + " -- " + view.toString());
+				Cursor cursor = getCursor();
 
 				LayoutInflater inflater = getLayoutInflater();
 				View row = inflater.inflate(R.layout.insulin_item, parent,
 						false);
 
-				final Map<String, String> data = (Map<String, String>) getItem(position);
-
+				Log.d("DB", "cursor(["+cursor.getCount()+"]["+cursor.getColumnCount()+"]) to ("+position+")");
+				
 				TextView label = (TextView) row.findViewById(R.id.InsulinName);
-				label.setText(data.get("insulinName"));
+				
+				String text = cursor.getString(cursor
+						.getColumnIndex(KEY_INSULIN_DESC));
+				label.setText(text);
+
+				text = cursor.getString(cursor.getColumnIndex(KEY_INSULIN_TSTRT));
 				label = (TextView) row.findViewById(R.id.WorkFrom);
-				label.setText(data.get("workFrom"));
+				label.setText(text);
+				text = cursor.getString(cursor.getColumnIndex(KEY_INSULIN_TEND));
 				label = (TextView) row.findViewById(R.id.WorkTo);
-				label.setText(data.get("workTo"));
+				label.setText(text);
+				text = cursor.getString(cursor.getColumnIndex(KEY_INSULIN_TMAX));
 				label = (TextView) row.findViewById(R.id.MaxFrom);
-				label.setText(data.get("maxFrom"));
+				label.setText(text);
+				text = cursor.getString(cursor.getColumnIndex(KEY_INSULIN_TWRK));
 				label = (TextView) row.findViewById(R.id.MaxTo);
-				label.setText(data.get("maxTo"));
+				label.setText(text);
+				
 				// label=(TextView)row.findViewById(R.id.Color);
 				// label.setText( data.get("Color") );
 
@@ -124,8 +174,8 @@ public class InsulinDescActivity extends Activity {
 
 		};
 
-		final SimpleAdapter simpleAdapter = new SimpleAdapter(this, commonData
-				.createInsulinList(), R.layout.insulin_item, from, to) {
+		final SimpleAdapter simpleAdapter = new SimpleAdapter(this,
+				commonData.createInsulinList(), R.layout.insulin_item, from, to) {
 
 			public View getView(int position, View convertView, ViewGroup parent) {
 				final View view = super.getView(position, convertView, parent);
@@ -186,47 +236,18 @@ public class InsulinDescActivity extends Activity {
 				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item
 				// R.layout.user_item // for this use only ArrayAdapter
 				);
+		cursorAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item
+				// R.layout.user_item // for this use only ArrayAdapter
+				);
 
-		Spinner spinner = (Spinner) findViewById(R.id.spinnerInsulins);
+		spinner = (Spinner) findViewById(R.id.spinnerInsulins);
 
-		//spinner.setAdapter(simpleAdapter);
+		// spinner.setAdapter(simpleAdapter);
 		spinner.setAdapter(cursorAdapter);
 
-		spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
-
-			/*
-			 * public OnItemSelectedListener(){ super(); }
-			 */
-			public void onItemSelected(AdapterView<?> parent, View view,
-					int position, long id) {
-				// Get the color name out of the Map
-/*				
-				final Map<String, String> data = (Map<String, String>) parent
-						.getItemAtPosition(position);
-
-				final String text = "Selected Insulin : "
-						+ data.get("insulinName");
-
-				Toast.makeText(parent.getContext(), text, Toast.LENGTH_LONG)
-						.show();
-
-				LayoutInflater inflater = getLayoutInflater();
-				View row = inflater.inflate(R.layout.insulin_desc, parent,
-						false);
-
-				editTextInsulinName.setText(data.get("insulinName"));
-				editTextInsulinFrom.setText(data.get("workFrom"));
-				editTextInsulinTo.setText(data.get("workTo"));
-				editTextInsulinMaxFrom.setText(data.get("maxFrom"));
-				editTextInsulinMaxTo.setText(data.get("maxTo"));
-				editTextInsulinColor.setText(data.get("color"));
-*/
-			}
-
-			public void onNothingSelected(AdapterView<?> arg0) {
-				// Do nothing
-			}
-		});
+		spinner.setOnItemSelectedListener(new InsulinOnItemSelectedListener() );
+		//spinner.setOnItemSelectedListener(new DataOnItemSelectedListener());
 
 	}
 
@@ -236,12 +257,118 @@ public class InsulinDescActivity extends Activity {
 	}
 
 	public void addInsulin(View v) {
-		dbHelper.insertInsulin(editTextInsulinName.getText().toString());
+		dbHelper.insertInsulin(
+				editTextInsulinName.getText().toString(),
+				editTextInsulinFrom.getText().toString(),
+				editTextInsulinTo.getText().toString(),
+				editTextInsulinMaxFrom.getText().toString(),
+				editTextInsulinMaxTo.getText().toString(),
+				editTextInsulinColor.getText().toString()
+				);
+		cursorAdapter.notifyDataSetChanged();
+		insulinCursor = dbHelper.getAlInsulins(); 
+		cursorAdapter.changeCursor( insulinCursor );
 	}
-	
+
 	public void delInsulin(View v) {
-		//dbHelper.deleteInsulin(rowId);
+		int rowId = insulinCursor.getInt(0);
+		Log.d("DB", "cursor ["+insulinCursor.getPosition()+"]["+rowId+"]");
+		dbHelper.deleteInsulin( rowId );
+		cursorAdapter.notifyDataSetChanged();
+		insulinCursor = dbHelper.getAlInsulins(); 
+		cursorAdapter.changeCursor( insulinCursor );
+	}
+
+	public void saveInsulin(View v) {
+		int rowId = insulinCursor.getInt(0);
+		dbHelper.updateInsulin(
+				rowId,
+				editTextInsulinName.getText().toString(),
+				editTextInsulinFrom.getText().toString(),
+				editTextInsulinTo.getText().toString(),
+				editTextInsulinMaxFrom.getText().toString(),
+				editTextInsulinMaxTo.getText().toString(),
+				editTextInsulinColor.getText().toString()
+				);
+		cursorAdapter.notifyDataSetChanged();
+		spinner.invalidate();
+		insulinCursor.requery();
+	}
+
+	public void viewDiagram(View v) {
+		// dbHelper.deleteInsulin(rowId);
 	}
 	
+	class InsulinOnItemSelectedListener implements OnItemSelectedListener {
+
+		public InsulinOnItemSelectedListener(){
+		}
+		
+		public void onItemSelected(AdapterView<?> parent, View view,
+				int position, long id) {
+			
+			Log.d("DB", "cursor(["+insulinCursor.getCount()+"]["+insulinCursor.getColumnCount()+"]) to ("+position+")("+insulinCursor.getPosition()+")");
+
+			TextView label = (TextView) findViewById(R.id.InsulinName);
+			String text = insulinCursor.getString(insulinCursor
+					.getColumnIndex(KEY_INSULIN_DESC));
+			label.setText(text);
+			editTextInsulinName.setText( text );
+			
+			text = insulinCursor.getString(insulinCursor.getColumnIndex(KEY_INSULIN_TSTRT));
+			editTextInsulinFrom.setText( text );
+			text = insulinCursor.getString(insulinCursor.getColumnIndex(KEY_INSULIN_TEND));
+			editTextInsulinTo.setText( text );
+			text = insulinCursor.getString(insulinCursor.getColumnIndex(KEY_INSULIN_TMAX));
+			editTextInsulinMaxFrom.setText( text );
+			text = insulinCursor.getString(insulinCursor.getColumnIndex(KEY_INSULIN_TWRK));
+			editTextInsulinMaxTo.setText( text );
+			text = insulinCursor.getString(insulinCursor.getColumnIndex(KEY_INSULIN_COLOR));
+			editTextInsulinColor.setText( text );
+			
+			
+		}
+
+		public void onNothingSelected(AdapterView<?> parent) {
+		}
+		
+	}
+	
+	
+	class DataOnItemSelectedListener implements OnItemSelectedListener {
+		/*
+		 * public OnItemSelectedListener(){ super(); }
+		 */
+		public void onItemSelected(AdapterView<?> parent, View view,
+				int position, long id) {
+			  // Get the color name out of the Map
+			
+			  final Map<String, String> data = (Map<String, String>) parent
+			 .getItemAtPosition(position);
+			  
+			  final String text = "Selected Insulin : " +
+			  data.get("insulinName");
+			  
+			  Toast.makeText(parent.getContext(), text, Toast.LENGTH_LONG)
+			  .show();
+			  
+			  LayoutInflater inflater = getLayoutInflater(); View row =
+			  inflater.inflate(R.layout.insulin_desc, parent, false);
+			  
+			  editTextInsulinName.setText(data.get("insulinName"));
+			  editTextInsulinFrom.setText(data.get("workFrom"));
+			  editTextInsulinTo.setText(data.get("workTo"));
+			  editTextInsulinMaxFrom.setText(data.get("maxFrom"));
+			  editTextInsulinMaxTo.setText(data.get("maxTo"));
+			  editTextInsulinColor.setText(data.get("color"));
+			 
+			
+		}
+
+		public void onNothingSelected(AdapterView<?> arg0) {
+			// Do nothing
+		}
+	}
+
 	
 }
---
+++ trunk/DiaMon/src/net/sf/dvstar/android/diamon/datastore/DBConst.java
@@ -0,0 +1,14 @@
+package net.sf.dvstar.android.diamon.datastore;
+
+public interface DBConst {
+
+	public static String KEY_ID = "_id";
+	public static String KEY_INSULIN_DESC  = "descript"; 
+	public static String KEY_INSULIN_TSTRT = "time_start"; 
+	public static String KEY_INSULIN_TEND  = "time_end"; 
+	public static String KEY_INSULIN_TMAX  = "time_max"; 
+	public static String KEY_INSULIN_TWRK  = "time_work"; 
+	public static String KEY_INSULIN_COLOR = "color";
+	
+	
+}
