c41468988f3e3ecddd150f25b69c6e128a4444ab
dmvstar@gmail.com <dmvstar@gmail.com@7170698d-7e1a-ea82-3c62-e3c85c3ce9f1>
2011-10-22 20:14:51 +0000

    update ui

--- trunk/DiaMon/src/net/sf/dvstar/android/diamon/common/CommonDescs.java
+++ trunk/DiaMon/src/net/sf/dvstar/android/diamon/common/CommonDescs.java
@@ -1,10 +1,12 @@
 package net.sf.dvstar.android.diamon.common;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CommonDescs {
 
-	public static class InsulinDesc{
+	public static class InsulinItem{
 		public double  time;
 		public int     dose;
 		public boolean compare(){
@@ -14,17 +16,37 @@ public class CommonDescs {
 		}
 	}
 
-	public static class InsulinDescList{
-		public ArrayList<InsulinDesc> list;
-		public InsulinDescList(){
-			list = new ArrayList<InsulinDesc>();
+	public static class Insulin{
+		public String  name;
+		public int     dose;
+		public double  start;
+		public double  max;
+		public double  min;
+		public double  end;
+		
+		public ArrayList<InsulinItem> list;
+		public Insulin(){
+			list = new ArrayList<InsulinItem>();
 		}
 		
-		public boolean addItem(InsulinDesc desc){
+		/**
+		 * 
+		 * @param dose dose of injection
+		 * @param begin time of injection
+		 * @return prepared hash
+		 */
+		public Map getInsulinActivity(int dose, double begin){
+			HashMap<?, ?> ret = new HashMap();
+			//ret.put(key, value)
+			return ret;
+		}
+		
+		public boolean addItem(InsulinItem desc){
 			boolean ret = false;
 			if(!list.contains(desc)) {list.add(desc); ret = true;}
 			return ret;
 		} 
+		
 	}
 	
 	
