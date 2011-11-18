b67d9b7754f3b18e76b9891bc07c4ed4857c84dc
dmvstar@gmail.com <dmvstar@gmail.com@7170698d-7e1a-ea82-3c62-e3c85c3ce9f1>
2011-11-18 17:10:59 +0000

    update ui

--- trunk/DiaMon/src/net/sf/dvstar/android/diamon/activities/AboutActivity.java
+++ trunk/DiaMon/src/net/sf/dvstar/android/diamon/activities/AboutActivity.java
@@ -3,14 +3,50 @@ package net.sf.dvstar.android.diamon.activities;
 import net.sf.dvstar.android.diamon.R;
 import net.sf.dvstar.android.diamon.R.layout;
 import android.app.Activity;
+import android.app.TabActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TabHost;
+import android.widget.TextView;
 
-public class AboutActivity extends Activity {
+public class AboutActivity extends TabActivity implements TabHost.TabContentFactory{
 
-    @Override
+    private TabHost mTabHost;
+
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
+        
+        
+    	mTabHost = getTabHost();
+		mTabHost.setup();
+
+		mTabHost.addTab(mTabHost
+				.newTabSpec("About")
+				.setIndicator("About",
+						getResources().getDrawable(R.drawable.star_big_on))
+				.setContent(this));
+		mTabHost.addTab(mTabHost.newTabSpec("Authors").setIndicator("Authors")
+				.setContent(this));
+		mTabHost.addTab(mTabHost.newTabSpec("Thanks").setIndicator("Thanks")
+				.setContent(this));
+		mTabHost.addTab(mTabHost.newTabSpec("License").setIndicator("License")
+				.setContent(this));
+		mTabHost.addTab(mTabHost.newTabSpec("Translate").setIndicator("Translate")
+				.setContent(this));
+
+		mTabHost.setCurrentTab(0);
+        
     }
+	
+	
+	public View createTabContent(String tag) {
+		final TextView tv = new TextView(this);
+		tv.setText("Content for tab with tag " + tag);
+		return tv;
+	}
+	
+	
 	
 }
